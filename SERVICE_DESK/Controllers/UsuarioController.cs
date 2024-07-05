6e1b688a424bffe728acff4c3d6a39b001158a2f
gean1987 <geancarlosvillafuerte18@gmail.com>
2024-07-04 19:35:33 -0500

    actualizacion ticketera

---
+++ SERVICE_DESK/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SERVICE_DESK.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Listado()
+        {
+            return View();
+        }
+    }
+}
